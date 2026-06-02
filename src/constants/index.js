373b5e066998bd41d92b3f004cbf65c11ef5ad12
Erfan Saeedian <erfuniism@gmail.com>
2026-06-02 10:28:56 +0330

    fiexed dashboard icon

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,7 +3,7 @@
 export const SIDEBAR_MENU_ITEMS = [
   {
     item: "Dashboard",
-    src: "../../../public/icons/dashboard.svg",
+    src: "https://raw.githubusercontent.com/ErfunisM/PharmacyManagementPanel-Database/80d5bf1a7c63359c6ee593b515214f5675dac767/Images/icons/dashboard.svg",
     link: "/dashboard",
   },
   {
