19b1f5f4814d3112fd309cb0b334955a5f03316e
Erfan Saeedian <erfuniism@gmail.com>
2026-06-02 10:46:17 +0330

    add avatar image

--- src/components/common/avatar.jsx
+++ src/components/common/avatar.jsx
@@ -2,7 +2,7 @@ const Avatar = () => {
   return (
     <img
       className="w-[50px] h-[50px] rounded-[50px] bg-amber-500"
-      src="../../../public/avatar.png"
+      src="avatar.png"
     />
   );
 };
